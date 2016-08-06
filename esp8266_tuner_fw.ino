5e6d68f777746ae239eae2c23cca9b84e8a2a6c8
Salil Tembe <salil2106@gmail.com>
2016-08-07 00:18:56 +0530

    Initial commit

--- esp8266_tuner_fw.ino
+++ esp8266_tuner_fw.ino
@@ -3,8 +3,8 @@
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 
-const char* ssid = "Tembe";
-const char* password = "salil24302199";
+const char* ssid = "...";
+const char* password = "...";
 String frequency = String("");
 String squelch = String("");
 ESP8266WebServer server(80);
